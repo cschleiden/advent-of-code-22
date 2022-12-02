dd3e8b62722106a882e1e616f6c7d448f6d480c1
Christopher Schleiden <cschleiden@live.de>
2022-12-01 21:10:03 -0800

    part2

--- day2/part1.ts
+++ day2/part1.ts
@@ -13,18 +13,6 @@ const shapes = {
     name: "Scissors",
     score: 3,
   },
-  X: {
-    name: "Rock",
-    score: 1,
-  },
-  Y: {
-    name: "Paper",
-    score: 2,
-  },
-  Z: {
-    name: "Scissors",
-    score: 3,
-  },
 };
 
 const outcome = {
@@ -50,7 +38,43 @@ const lines = await getLines("./input.txt");
 let score = 0;
 
 for (const line of lines) {
-  const [player1, player2] = line.split(" ");
+  const [player1, desiredOutcome] = line.split(" ");
+
+  let player2: string = "";
+
+  switch (desiredOutcome) {
+    case "X": // Lose
+      switch (shapes[player1].name) {
+        case "Rock":
+          player2 = "C";
+          break;
+        case "Paper":
+          player2 = "A";
+          break;
+        case "Scissors":
+          player2 = "B";
+          break;
+      }
+      break;
+
+    case "Y": // Draw
+      player2 = player1;
+      break;
+
+    case "Z": // Win
+      switch (shapes[player1].name) {
+        case "Rock":
+          player2 = "B";
+          break;
+        case "Paper":
+          player2 = "C";
+          break;
+        case "Scissors":
+          player2 = "A";
+          break;
+      }
+      break;
+  }
 
   score += shapes[player2].score + calcScore(calcOutcome(player1, player2));
 }
