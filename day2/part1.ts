af81ba19b0f18e8aecf8e365c0dab36b113f85b5
Christopher Schleiden <cschleiden@live.de>
2022-12-01 21:07:23 -0800

    part 1

--- day2/part1.ts
+++ day2/part1.ts
@@ -1,4 +1,73 @@
-import { readFile } from "fs/promises";
+import { getLines } from "../lib/fs";
 
-const input = await readFile("input.txt", "utf-8");
-const lines = input.split("\n");
+const shapes = {
+  A: {
+    name: "Rock",
+    score: 1,
+  },
+  B: {
+    name: "Paper",
+    score: 2,
+  },
+  C: {
+    name: "Scissors",
+    score: 3,
+  },
+  X: {
+    name: "Rock",
+    score: 1,
+  },
+  Y: {
+    name: "Paper",
+    score: 2,
+  },
+  Z: {
+    name: "Scissors",
+    score: 3,
+  },
+};
+
+const outcome = {
+  Rock: {
+    Rock: 0,
+    Paper: 1,
+    Scissors: -1,
+  },
+  Paper: {
+    Rock: -1,
+    Paper: 0,
+    Scissors: 1,
+  },
+  Scissors: {
+    Rock: 1,
+    Paper: -1,
+    Scissors: 0,
+  },
+};
+
+const lines = await getLines("./input.txt");
+
+let score = 0;
+
+for (const line of lines) {
+  const [player1, player2] = line.split(" ");
+
+  score += shapes[player2].score + calcScore(calcOutcome(player1, player2));
+}
+
+console.log(score);
+
+function calcOutcome(player1: string, player2: string): number {
+  return outcome[shapes[player1].name][shapes[player2].name];
+}
+
+function calcScore(outcome: number): number {
+  switch (outcome) {
+    case -1:
+      return 0;
+    case 0:
+      return 3;
+    case 1:
+      return 6;
+  }
+}
