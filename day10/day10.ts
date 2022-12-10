7651a2e7c0cdb2b31129e58ac5af43c8844d2972
Christopher Schleiden <cschleiden@live.de>
2022-12-09 21:46:59 -0800

    day10 - part 2

--- day10/day10.ts
+++ day10/day10.ts
@@ -16,14 +16,27 @@ let cycle = 0;
 
 let signal = 0;
 
+let output = [];
 for (const line of lines) {
   const [command, value] = line.split(" ");
 
   const { cycles } = commands[command];
   for (let i = 0; i < cycles; i++) {
+    // Draw
+    const pixel = cycle % 40;
+    if (regX - 1 <= pixel && pixel <= regX + 1) {
+      output.push("#");
+    } else {
+      output.push(".");
+    }
+    if (output.length >= 40) {
+      console.log(output.join(""));
+      output = [];
+    }
+
     cycle++;
     if ([20, 60, 100, 140, 180, 220].includes(cycle)) {
-      console.log(cycle, regX);
+      // console.log(cycle, regX);
       signal += cycle * regX;
     }
 
