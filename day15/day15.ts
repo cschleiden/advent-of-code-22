cf91e5886dd0638427a967d44419acd19bb42362
Christopher Schleiden <cschleiden@github.com>
2022-12-15 12:02:40 +0100

    day15 - part 2

--- day15/day15.ts
+++ day15/day15.ts
@@ -35,27 +35,115 @@ for (const line of lines) {
   maxX = Math.max(maxX, p.x + d);
 }
 
-//
-const targetY = 2000000;
-let p = 0;
-let o = [];
-for (let x = minX - 1; x < maxX + 1; x++) {
-  if (
-    sensors.some(
-      (s) =>
-        dist({ x, y: targetY }, s.p) <= s.dist &&
-        (s.b.x !== x || s.b.y !== targetY)
-    )
-  ) {
-    // o.push("#");
-    p++;
-  } else {
-    // o.push(".");
-  }
+// //
+// const targetY = 2000000;
+// let p = 0;
+// let o = [];
+// for (let x = minX - 1; x < maxX + 1; x++) {
+//   if (
+//     sensors.some(
+//       (s) =>
+//         dist({ x, y: targetY }, s.p) <= s.dist &&
+//         (s.b.x !== x || s.b.y !== targetY)
+//     )
+//   ) {
+//     // o.push("#");
+//     p++;
+//   } else {
+//     // o.push(".");
+//   }
+// }
+
+type Rect = {
+  start: Pos;
+  length: number;
+};
+
+const rects: Rect[] = [];
+for (const s of sensors) {
+  const top = s.p.y - s.dist;
+  const left = s.p.x - s.dist;
+  const right = s.p.x + s.dist;
+
+  const start = rot({ x: s.p.x, y: top });
+  const r: Rect = {
+    start: start,
+    length: rot({ x: right, y: s.p.y }).x - start.x,
+  };
+  rects.push(r);
 }
 
-console.log(o.join(""));
-console.log(p);
+function rot(p: Pos): Pos {
+  return {
+    x: p.x + p.y,
+    y: p.y - p.x,
+  };
+}
+
+let searchSpace =
+  process.argv[2].indexOf("example.txt") !== -1 ? 20 : 4_000_000;
+
+let x, y;
+
+outer: for (const s of sensors) {
+  let leftX = s.p.x - s.dist - 1;
+  let rightX = s.p.x + s.dist + 1;
+  let topY = s.p.y - s.dist - 1;
+  let bottomY = s.p.y + s.dist + 1;
+
+  // right, down
+  x = s.p.x;
+  y = topY;
+  while (x <= rightX && y <= s.p.y) {
+    if (isFree({ x, y })) {
+      console.log("Found at", x, y);
+      break outer;
+    }
+    x++;
+    y++;
+  }
+
+  while (x >= s.p.x && y <= bottomY) {
+    if (isFree({ x, y })) {
+      console.log("Found at", x, y);
+      break outer;
+    }
+    x--;
+    y++;
+  }
+
+  while (x >= leftX && y >= s.p.y) {
+    if (isFree({ x, y })) {
+      console.log("Found at", x, y);
+      break outer;
+    }
+    x--;
+    y--;
+  }
+
+  while (x <= s.p.x && y <= topY) {
+    if (isFree({ x, y })) {
+      console.log("Found at", x, y);
+      break outer;
+    }
+    x++;
+    y--;
+  }
+
+  console.log("Sensor done");
+}
+
+console.log("Found at", x, y, "Frequency", x * 4_000_000 + y);
+
+function isFree(p: Pos): boolean {
+  return (
+    p.x >= 0 &&
+    p.x <= searchSpace &&
+    p.y >= 0 &&
+    p.y <= searchSpace &&
+    sensors.every((s) => dist(p, s.p) > s.dist)
+  );
+}
 
 // Manhattan distance
 function dist(s: Pos, t: Pos): number {
