ecfe891c2553f0f6c02c9adf82614ed248d93937
Christopher Schleiden <cschleiden@github.com>
2022-12-15 12:03:00 +0100

    Remove unused code

--- day15/day15.ts
+++ day15/day15.ts
@@ -35,51 +35,6 @@ for (const line of lines) {
   maxX = Math.max(maxX, p.x + d);
 }
 
-// //
-// const targetY = 2000000;
-// let p = 0;
-// let o = [];
-// for (let x = minX - 1; x < maxX + 1; x++) {
-//   if (
-//     sensors.some(
-//       (s) =>
-//         dist({ x, y: targetY }, s.p) <= s.dist &&
-//         (s.b.x !== x || s.b.y !== targetY)
-//     )
-//   ) {
-//     // o.push("#");
-//     p++;
-//   } else {
-//     // o.push(".");
-//   }
-// }
-
-type Rect = {
-  start: Pos;
-  length: number;
-};
-
-const rects: Rect[] = [];
-for (const s of sensors) {
-  const top = s.p.y - s.dist;
-  const left = s.p.x - s.dist;
-  const right = s.p.x + s.dist;
-
-  const start = rot({ x: s.p.x, y: top });
-  const r: Rect = {
-    start: start,
-    length: rot({ x: right, y: s.p.y }).x - start.x,
-  };
-  rects.push(r);
-}
-
-function rot(p: Pos): Pos {
-  return {
-    x: p.x + p.y,
-    y: p.y - p.x,
-  };
-}
-
 let searchSpace =
   process.argv[2].indexOf("example.txt") !== -1 ? 20 : 4_000_000;
 
